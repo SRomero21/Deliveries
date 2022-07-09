aef94222e4391cc2fb31cad6c804692dd339f146
Cj Corp Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-07-08 19:05:33 -0500

    actualizaciones en las peticionas Get

---
+++ api/routers/services/get.php
@@ -0,0 +1,93 @@
+<?php
+  require_once "controllers/get.controller.php";
+  $table= explode("?",$arrayRouters[2])[0];
+  $select=$_GET["select"]?? "*";
+  $orderBy=$_GET["orderBy"]?? null;
+  $orderMode=$_GET["orderMode"]?? null;
+  $startAt=$_GET["startAt"]?? null;
+  $endAt=$_GET["endAt"]?? null;
+  $filterTo=$_GET["filterTo"]?? null;
+  $inTo=$_GET["inTo"]?? null;
+  $response = new GetController();
+  /***************************************
+  ** Petición GET sin orden sin limites
+  ****************************************/
+  if (!isset($_GET["orderBy"]) && !isset($_GET["orderMode"])&&!isset($_GET["startAt"]) && !isset($_GET["endAt"])) {
+    $response -> getData($table, $select, $orderBy, $orderMode, $startAt, $endAt);
+  } else
+  /***************************************
+  ** Petición GET con orden sin limites
+  ****************************************/
+  if (isset($_GET["orderBy"]) && isset($_GET["orderMode"]) && !isset($_GET["startAt"]) && !isset($_GET["endAt"])) {
+    $response->getData($table, $select,$_GET["orderBy"], $_GET["orderMode"], $startAt, $endAt);
+  }else
+  /***************************************
+   ** Petición GET sin orden con limites
+   ****************************************/
+  if (!isset($_GET["orderBy"]) && !isset($_GET["orderMode"]) && isset($_GET["startAt"]) && isset($_GET["endAt"])) {
+    $response->getData($table, $select, $orderBy, $orderMode, $_GET["startAt"], $_GET["endAt"]);
+  }else
+  /***************************************
+   ** Petición GET con orden con limites
+   ****************************************/
+  if (isset($_GET["orderBy"]) && isset($_GET["orderMode"]) && isset($_GET["startAt"]) && isset($_GET["endAt"])) {
+    $response->getData($table, $select, $_GET["orderBy"], $_GET["orderMode"], $_GET["startAt"], $_GET["endAt"]);
+  }else
+  /*******************************
+  *! Con filtro
+  ********************************/
+  /**************************************
+  ** Petición Get con filtro sin orden
+  ***************************************/
+  if(!isset($_GET["orderBy"])&& !isset($_GET["orderMode"]) && isset($_GET["linkTo"])&& isset($_GET["equalTo"])&& !isset($_GET["startAt"]) && !isset($_GET["endAt"])){
+    $response -> getDataFilter($table, $select, $_GET["linkTo"], $_GET["equalTo"], $orderBy, $orderMode, $startAt, $endAt);
+  }else
+  /****************************************
+  ** Petición get con filtro con orden.
+  *****************************************/
+  if(isset($_GET["linkTo"])&& isset($_GET["equalTo"]) && isset($_GET["orderBy"])&& isset($_GET["orderMode"])){
+    $response -> getDataFilter($table, $select, $_GET["linkTo"], $_GET["equalTo"], $_GET["orderBy"],$_GET["orderMode"], $startAt, $endAt);
+  }else
+  /****************************************
+   ** Petición get sin filtro con orden.
+   *****************************************/
+  if (!isset($_GET["linkTo"]) && !isset($_GET["equalTo"]) && isset($_GET["orderBy"]) && isset($_GET["orderMode"])) {
+    $response->getDataFilter($table, $select, $linkTo, $equalTo, $_GET["orderBy"], $_GET["orderMode"], $startAt, $endAt);
+  } else
+  /************************************************************
+  ** Peticiones Get sin filtros entre tablas relacionadas.
+  *************************************************************/
+  if(isset($_GET["rel"]) && isset($_GET["type"]) && $table=="relations" && !isset($_GET["linkTo"]) && !isset($_GET["equalTo"])){
+    $response -> getRelData($_GET["rel"], $_GET["type"], $select, $orderBy, $orderMode, $startAt, $endAt);
+  }else
+  /************************************************************
+  ** Peticiones Get con filtros entre tablas relacionadas.
+  *************************************************************/
+  if(isset($_GET["rel"]) && isset($_GET["type"]) && $table=="relations" && isset($_GET["linkTo"]) && isset($_GET["equalTo"])){
+    $response -> getRelDataFilter($_GET["rel"], $_GET["type"], $select, $_GET["linkTo"], $_GET["equalTo"], $orderBy, $orderMode, $startAt, $endAt);
+  }else
+  /****************************************************
+  ** Peticiones Get para buscadores sin relaciones
+  *****************************************************/
+  if(!isset($_GET["rel"]) && !isset($_GET["type"]) && isset($_GET["linkTo"]) && isset($_GET["searchTo"])) {
+    $response->getDataFilter($table, $select, $_GET["linkTo"], $_GET["searchTo"], $orderBy, $orderMode, $startAt, $endAt);
+  }else
+  /**************************************************************
+  ** Peticiones Get para buscadores entre tablas relacionadas.
+  ***************************************************************/
+  if (isset($_GET["rel"]) && isset($_GET["type"]) && $table == "relations" && isset($_GET["linkTo"]) && isset($_GET["searchTo"])) {
+    $response->getRelDataFilter($_GET["rel"], $_GET["type"], $select, $_GET["linkTo"], $_GET["searchTo"], $orderBy, $orderMode, $startAt, $endAt);
+  }else
+  /************************************************************
+  ** Peticiones Get para selección de rangos.
+  *************************************************************/
+  if (!isset($_GET["rel"]) && !isset($_GET["type"]) && isset($_GET["linkTo"]) && isset($_GET["betweenIn"]) && isset($_GET["betweenOut"])) {
+    $response->getDataRange($table, $select, $_GET["linkTo"], $_GET["betweenIn"], $_GET["betweenOut"], $orderBy, $orderMode, $startAt, $endAt, $filterTo, $inTo);
+  }else
+  /****************************************************************
+  ** Peticiones Get para selección de rangos. con relaciones.
+  *****************************************************************/
+  if (isset($_GET["rel"]) && isset($_GET["type"]) && $table == "relations" && isset($_GET["linkTo"]) && isset($_GET["betweenIn"]) && isset($_GET["betweenOut"])) {
+    $response->getRelDataRange($_GET["rel"],$_GET["type"], $select, $_GET["linkTo"], $_GET["betweenIn"], $_GET["betweenOut"], $orderBy, $orderMode, $startAt, $endAt, $filterTo, $inTo);
+  }
+?>
